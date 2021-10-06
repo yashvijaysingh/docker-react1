624bdab49e07aa8f1c892c5e30e32ba93ef1373f
yashvijaysingh <yash.vs.iter@gmail.com>
2021-10-06 11:41:13 +0530

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn java/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn java/i);
+  expect(linkElement).toBeInTheDocument();
+});
